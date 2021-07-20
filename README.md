b6164105f9727e03520c45031be82c9afb1d9060
justinjabate <justin.j.abate@gmail.com>
2021-07-20 13:00:25 -0400

    more cleanup

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 Verilog implementation of an AXIS number sorter
 
 
+To run:
+```
+cd sim/xsim
+./test_runner.sh
+```
+
 The sorter module receives four numbers from the AXIS bus and presents them on output ports lvl1, lvl2, lvl3, and lvl4, where level 1 is the largest value and level 4 is the smallest value. A done flag is asserted when lvl1-4 are valid.
 
 
-Simulatuion passed with DATA_WIDTH 16, 32, and 64. To run, cd to sim/xsim and run ./sorter_tb.sh
+Simulatuion passed with DATA_WIDTH 16, 32, and 64. 
 
 
-Platform: Vivado 2019.2 on Ubuntu 18.04.4 LTS
+: Vivado 2019.2 on Ubuntu 18.04.4 LTS
--- sim/xsim/test_runner.sh
+++ sim/xsim/test_runner.sh
@@ -8,38 +8,20 @@
 #
 #*********************************************************************************************************
 
-DUT="sorter_tb"
-
-# Command line options
+# Options
+TB="sorter_tb"
 xv_boost_lib_path=/opt/xlnx/Vivado/2019.2/tps/boost_1_64_0
 xvlog_opts="--relax"
 xvhdl_opts="--relax"
+xelab_opts="--relax --debug typical --mt auto -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot $TB xil_defaultlib.$TB xil_defaultlib.glbl"
 
 
-# Compile design files
-compile()
-{
-  xvlog $xvlog_opts -prj ./src/vlog.prj -log ./logs/log_01_xvlog.log
-  xvhdl $xvhdl_opts -prj ./src/vhdl.prj -log ./logs/log_02_xvhdl.log
-}
-
-elaborate()
-{
-  xelab --relax --debug typical --mt auto -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot sorter_tb xil_defaultlib.sorter_tb xil_defaultlib.glbl -log ./logs/log_03_xelab.log
-}
-
-simulate()
-{
-  xsim sorter_tb -gui -key {Behavioral:sim_1:Functional:sorter_tb} -tclbatch ./src/cmd.tcl -log ./logs/log_04_xsim.log
-}
-
-# STEP: setup
 setup()
 {
   case $1 in
     "-lib_map_path" )
       if [[ ($2 == "") ]]; then
-        echo -e "ERROR: Simulation library directory path not specified (type \"./sorter_tb.sh -help\" for more information)\n"
+        echo -e "ERROR: Simulation library directory path not specified (type \"./test_runner.sh -help\" for more information)\n"
         exit 1
       fi
     ;;
@@ -56,7 +38,7 @@ setup()
 }
 
 
-cleanup()
+delete_records()
 {
   rm -rf .Xil/ xsim.dir/
 #  find ./ -maxdepth 1 -type f -name "*.log" -exec mv {} logs/ \;
@@ -68,26 +50,26 @@ cleanup()
 
 }
 
-# Delete generated data from the previous run
-# reset_run()
-# {
-#   files_to_remove=(xelab.pb xsim.jou xvhdl.log xvlog.log compile.log elaborate.log simulate.log xelab.log xsim.log run.log xvhdl.pb xvlog.pb sorter_tb.wdb xsim.dir)
-#   for (( i=0; i<${#files_to_remove[*]}; i++ )); do
-#     file="${files_to_remove[i]}"
-#     if [[ -e $file ]]; then
-#       rm -rf $file
-#     fi
-#   done
-#   rm -rf logs/
-# }
 
+# Delete generated data from the previous run
+reset_run()
+{
+  files_to_remove=(xelab.pb xsim.jou xvhdl.log xvlog.log compile.log elaborate.log simulate.log xelab.log xsim.log run.log xvhdl.pb xvlog.pb $TB.wdb xsim.dir)
+  for (( i=0; i<${#files_to_remove[*]}; i++ )); do
+    file="${files_to_remove[i]}"
+    if [[ -e $file ]]; then
+      rm -rf $file
+    fi
+  done
+  rm -rf logs/
+}
 
 
 # Check command line arguments
 check_args()
 {
   if [[ ($1 == 1 ) && ($2 != "-lib_map_path" && $2 != "-noclean_files" && $2 != "-reset_run" && $2 != "-help" && $2 != "-h") ]]; then
-    echo -e "ERROR: Unknown option specified '$2' (type \"./sorter_tb.sh -help\" for more information)\n"
+    echo -e "ERROR: Unknown option specified '$2' (type \"./test_runner.sh -help\" for more information)\n"
     exit 1
   fi
 
@@ -96,13 +78,12 @@ check_args()
   fi
 }
 
-# Script usage
+
 usage()
 {
-  msg="Usage: sorter_tb.sh [-help]\n\
-Usage: sorter_tb.sh [-lib_map_path]\n\
-Usage: sorter_tb.sh [-reset_run]\n\
-Usage: sorter_tb.sh [-noclean_files]\n\n\
+  msg="test_runner.sh [-lib_map_path]\n\
+test_runner.sh [-reset_run]\n\
+test_runner.sh [-noclean_files]\n\n\
 [-help] -- Print help information for this script\n\n\
 [-lib_map_path <path>] -- Compiled simulation library directory path. The simulation library is compiled\n\
 using the compile_simlib tcl command. Please see 'compile_simlib -help' for more information.\n\n\
@@ -115,19 +96,23 @@ from the previous run will be removed. If you don't want to remove the simulator
 }
 
 
-# Main steps
 run()
 {
   check_args $# $1
   setup $1 $2
   mkdir -p logs/
-  compile
-  elaborate
-  simulate
-  cleanup
+  # Compile
+  xvlog $xvlog_opts -prj ./src/vlog.prj -log ./logs/log_01_xvlog.log
+  xvhdl $xvhdl_opts -prj ./src/vhdl.prj -log ./logs/log_02_xvhdl.log
+  # Elaborate
+  xelab $xelab_opts -log ./logs/log_03_xelab.log
+  # Simulate
+  xsim $TB -gui -key {Behavioral:sim_1:Functional:$TB} -tclbatch ./src/cmd.tcl -log ./logs/log_04_xsim.log
+  # Clean
+  delete_records
 }
 
 
-# Launch script
-echo -e ">>>>> test_runner.sh via Vivado Xsim export_simulation"
+# Launch
+echo -e ">>>>>> test_runner.sh via Vivado Xsim export_simulation"
 run $1 $2
